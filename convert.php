c65c511b72ee7cf01c7920f2b05b092536c1c40b
ethanejohnsons <ethan@ejohnsons.com>
2020-10-27 23:00:06 -0500

    Got most of the functionality up and running

---
+++ convert.php
@@ -0,0 +1,11 @@
+<?php
+
+function convertKMtoMM($km) {
+    return $km * 1000000;
+}
+
+function convertMMtoKM($mm) {
+	return $mm / 1000000;
+}
+
+?>
--- index.php
+++ index.php
@@ -1,69 +1,97 @@
 <?php
+	include 'convert.php';
+	date_default_timezone_set("America/Chicago");
 	extract($_REQUEST);
 
-	if ($submit==NULL) {
-		$guesses = array();
-		$number = 0;
-		$randomnumber = rand(1,1000);
+	$date = new DateTime();
+	if ($km!=NULL) {
+		$mm = convertKMtoMM($km);
+		$history[] = array($mm, '<-', $km, $date->getTimestamp());
+	} else if ($mm!=NULL) {
+		$km = convertMMtoKM($mm);
+		$history[] = array($mm, '->', $km, $date->getTimestamp());
 	} else {
-		$number++;
-		$guesses[] = $guess;
+		$history = array();
 	}
 
-	function echoGuesses($guesses, $randomnumber) {
-		foreach ($guesses as $g) {
-			echo $g;
-			if ($g > $randomnumber)
-				echo " > ";
-			else if ($g < $randomnumber)
-				echo " < ";
-			else 
-				echo " is ";
-			echo " the random number<br>";
+	$showSource = False;
+	if ($source!=NULL) {
+		$showSource = !$showSource;
+	}
+
+	function echoHistory($history) {
+		for ($i=0; $i<count($history); $i++) {
+			if ($history[$i][1] == '<-') {
+				echo <<< HERE
+					<a href="javascript:{}" onclick="document.getElementById('km').submit();">
+				HERE;
+				echo strval($history[$i][0])."mm from ".strval($history[$i][2])."km ";
+			} else if ($history[$i][1] == '->') {
+				echo <<< HERE
+					<a href="javascript:{}" onclick="document.getElementById('mm').submit();">
+				HERE;
+				echo strval($history[$i][2])."km from ".strval($history[$i][0])."mm ";
+			}
+
+			echo strval(date("h:i", $history[$i][3]))."</a><br>";
 		}
 	}
 
+	function serializeHistory($history) {
+		foreach ($history as $key => $val) {
+        	foreach ($val as $k => $v) {
+            	echo <<< HERE
+					<input type="hidden" name="history[$key][$k]" value="$v">
+HERE;
+            }
+        }
+	}
+
 	echo <<< HERE
-        <form action="KMtoMM.php" method="post">
-            <p>Kilometers: <input type="text" name="km"/></p>
-            <p><input type="submit"/></p>
-        </form>
-
-        <form action="MMtoKM.php" method="post">
-            <p>Millimeters: <input type="text" name="mm"/></p>
-            <p><input type="submit"/></p>
-        </form>
-
-		<h1>Guess a number between 1 and 1000</h1><br>
+		<h1>Convert between Millimeters and Kilometers</h1><br>
 
 		<table border="2">
 			<tbody>
 				<tr>
 					<td>
-						<form action="index.php" method="post">
-						Guess: <input type="text" name="guess">
+        				<form action="index.php" method="post" id="km">
 HERE;
 
-		for ($i=0; $i<count($guesses); $i++) {
-    		echo <<< HERE
-				<input type="hidden" name="guesses[$i]" value=$guesses[$i]>
-HERE;
-		}
+	serializeHistory($history);
+	if ($km!=NULL) {
+    	echo "<p>Kilometers: <input type='text' name='km' value=$km></p>";
+	} else {
+    	echo "<p>Kilometers: <input type='text' name='km'></p>";
+	}
 
 	echo <<< HERE
-						<input type="hidden" name="randomnumber" value=$randomnumber>
-						<input type="hidden" name="number" value=$number>
-						<input type="submit" name="submit" value="submit"><br>
-						<input type="submit" name="startover" value="start over"><br>
-						</form>
+        					<p><input type="submit"/></p>
+        				</form>
 
-						<h2>Number of Guesses: $number</h2></td>
-					</td>
-					<td>
-						<h2>Guesses</h2><br>
+        				<form action="index.php" method="post" id="mm">
 HERE;
 
-	echoGuesses($guesses, $randomnumber);
+	serializeHistory($history);
+	if ($mm!=NULL) {
+		echo "<p>Millimeters: <input type='text' name='mm' value=$mm></p>";
+	} else {
+		echo "<p>Millimeters: <input type='text' name='mm'></p>";
+	}
+
+	echo <<< HERE
+            				<p><input type="submit"/></p>
+        				</form>
+
+						<form action="index.php" method="post">
+							<p><input type="submit" name = "source" value="Show Source"/></p>
+							<input type="hidden" name="showSource" value=$showSource>
+						</form>
+					</td>
+					<td>
+						<h2>History</h2><br>
+HERE;
+
+	echoHistory($history);
 	echo <<< HERE
 					</td>
 				</tr>
@@ -71,10 +99,10 @@ HERE;
 		</table>
 HERE;
 
-	if ($guess == $randomnumber)
-		echo "<h1>CORRECT</h1>";
-
 	echo "<a href='..'>Home</a>";
 	echo "<HR>";
-	highlight_file("index.php");
+
+	if ($showSource) {
+		highlight_file("index.php");
+	}
 ?>
